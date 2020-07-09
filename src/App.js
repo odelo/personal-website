e6848a72a04aeb2f03a23be74c658a73e617abde
odelo <regan.col@northeastern.edu>
2020-07-08 22:55:00 -0400

    Added personal picture and intitial formatting change. This will be v1 until the next major stylistic update.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
 import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
-import Figure from "react-bootstrap/Figure";
-import Carousel from "react-bootstrap/Carousel";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import Image from "react-bootstrap/Image";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
 import Pdf from "./files/resume.pdf";
 import Github from "./files/github.png";
 import Insta from "./files/instagram.png";
 import LinkedIn from "./files/linkedin.png";
+import Headshot from "./files/headshot.jpg";
 
 function NavigationBar() {
   return (
@@ -56,26 +61,38 @@ function App() {
     <div className="App">
       <NavigationBar />
       <header className="App-header">
-        <div>
-          <p class="section-title">Welcome, my name is </p>
-          <p class="name">Colin Regan.</p>
-          <div class="section-body">
-            <p>
-              This is my pet project—a website I built as an excuse to learn
-              some new things and show off what I like to do. Feel free to take
-              a look.
-            </p>
-          </div>
-        </div>
-        <Figure>
-          <Figure.Image
-            width={100}
-            height={100}
-            alt="100x100"
-            src="headshot.jpg/100x100"
-          ></Figure.Image>
-        </Figure>
+        <Container fluid>
+          <Row>
+            <Col xs={8}>
+              <p class="section-title">Welcome, my name is</p>
+              <p class="name">Colin Regan</p>
+              <Row>
+                <Col>
+                  <div className="rectangle"></div>
+                </Col>
+              </Row>
+              <div class="section-body">
+                <p>
+                  This is my pet project—a website I built as an excuse to learn
+                  some new things and show off what I like to do. Feel free to
+                  take a look.
+                </p>
+              </div>
+            </Col>
+            <Col xs={3}>
+              <div className="header-image">
+                <Image
+                  style={{ height: "auto", width: 500 }}
+                  src={Headshot}
+                  roundedCircle
+                ></Image>
+              </div>
+            </Col>
+          </Row>
+          <Row></Row>
+        </Container>
       </header>
+      <main className="App-main"></main>
     </div>
   );
 }
